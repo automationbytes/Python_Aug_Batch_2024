434dbb75aaa574408bf1d14436179071b90cccee
automationbytes <automationbytes@gmail.com>
2024-08-24 20:26:36 +0530

    Added Day 5

---
+++ Day5/Modules_Eg.py
@@ -0,0 +1,32 @@
+'''
+
+Modules - something a code library, which has set of functions which we can use it
+
+'''
+
+def greeting(name):
+    return "Hello "+name
+
+
+stud1 = {
+    "name":"Tom",
+    "age": 5
+}
+
+
+stud2 = {
+    "name":"Jerry",
+    "age": 5
+}
+
+
+stud3 = {
+    "name":"Mahi",
+    "age": 5
+}
+
+def sum(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
---
+++ Day5/callingMod.py
@@ -0,0 +1,15 @@
+#from Modules_Eg import stud1 as s
+
+#print(s["name"])
+#
+# import Modules_Eg
+#
+# print(Modules_Eg.greeting("Hello"))
+# print(Modules_Eg.stud1["name"])
+
+
+import Modules_Eg as m
+
+print(m.greeting("Hello")) #func
+print(m.stud1["name"]) #dic
+
