7679a26d7a43e8d4aa68bdc13fd6f2f7f22a9922
automationbytes <automationbytes@gmail.com>
2024-09-21 20:31:37 +0530

    Webtable

---
+++ Day11/WebTable_4.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+options.add_argument("--start-maximized")
+options.add_argument("--disable-notifications")
+options.add_argument("--safebrowsing-disable-download-protection")
+prefs = {"download.default_directory": os.getcwd()}
+options.add_experimental_option("prefs",prefs)
+
+
+driver = webdriver.Chrome(options = options)
+driver.get("https://sriayyanarcrackers.in/estimate.php")
+time.sleep(2)
+row = driver.find_elements(By.XPATH, '//table[@class="estimate-table"]//tr')
+rowsize = len(row)
+print("Number of rows:", rowsize)
+
+col = driver.find_elements(By.XPATH, '//table[@class="estimate-table"]//th')
+colsize = len(col)
+print("Number of columns:", colsize)
+
+header_mapping = {}
+for j in range(1, colsize + 1):
+    header = driver.find_element(By.XPATH, f'//table[@class="estimate-table"]//th[{j}]').text
+    header_mapping[header] = j
+print("Header-Column Mapping:", header_mapping)
+
+for i in range(1, rowsize):
+    product_name_elements = driver.find_elements(By.XPATH, f'//table[@class="estimate-table"]//tr[{i}]//td[2]')
+
+    if product_name_elements:
+        productName = product_name_elements[0].text
+        if productName == "Pop star (1pcs)":
+            input_column_index = header_mapping["Quantity"]
+
+            driver.find_element(By.XPATH,
+                                f'//table[@class="estimate-table"]//tr[{i}]//td[{input_column_index}]//input[@type="text"]').clear()
+            driver.find_element(By.XPATH,
+                                f'//table[@class="estimate-table"]//tr[{i}]//td[{input_column_index}]//input[@type="text"]').send_keys("5")
+            break
--- Day11/WebTables_1.py
+++ Day11/WebTables_1.py
@@ -1,6 +1,5 @@
 import os
 import time
-
 from selenium import webdriver
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common import by
@@ -8,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 options.add_argument("--start-maximized")
@@ -19,20 +17,20 @@ options.add_experimental_option("prefs",prefs)
 
 
 driver = webdriver.Chrome(options = options)
-driver.get("https://sriayyanarcrackers.in/estimate.php")
+driver.get("https://demo.guru99.com/test/web-table-element.php")
 time.sleep(2)
 
-row = driver.find_elements(By.XPATH,'//table[@class="estimate-table"]//tr')
+
+row = driver.find_elements(By.XPATH,'//table[@class="dataTable"]//tr')
 rowsize = len(row)
 print(rowsize)
 
 #//table[@class="estimate-table"]//tr//td[2]
 
 for i in range(1, rowsize):
-    # Find the second <td> in the current row
-    product_name_elements = driver.find_elements(By.XPATH, f'//table[@class="estimate-table"]//tr[{i}]//td[2]')
-
-    # Check if the second <td> exists
-    if product_name_elements:
-        productName = product_name_elements[0].text
-        print(productName)
+    productName = driver.find_element(By.XPATH,f'//table[@class="dataTable"]//tr[{i}]//td/a').text
+    print(productName)
+    if productName == "Ujjivan Financial":
+        CurrentPrice = driver.find_element(By.XPATH, f'//table[@class="dataTable"]//tr[{i}]//td[4]').text
+        print(CurrentPrice)
+        break
